d9d0a4c8f87ace28ea4adbb7150bab7ce78ba7fb
Erivan Muniz Machado <erivanmuniz@outlook.com>
2024-02-12 16:46:54 -0300

    Tratando excessões na classe Leitura de dados do usuário

--- src/LeituraDeDados.java
+++ src/LeituraDeDados.java
@@ -1,20 +1,29 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class LeituraDeDados {
-    public static void main(String[] args) {
+        public static void main (String[]args){
+            Scanner leitura = new Scanner(System.in);
 
-        Scanner leitura = new Scanner(System.in);
+            int numeroDigitado = 0;
+            boolean numeroValido = false;
 
-        System.out.print("Digite um número: ");
+            while (!numeroValido) {
+                try {
+                    System.out.print("Digite um número: ");
 
+                    numeroDigitado = leitura.nextInt();
+                    numeroValido = true;
 
-        if (leitura.hasNextInt()) {
-            int numeroDigitado = leitura.nextInt();
+                } catch (InputMismatchException e) {
+                    System.out.println("Por favor, digite um número inteiro válido.");
+                    leitura.nextLine();
+                }
+            }
             System.out.println("Você digitou: " + numeroDigitado);
-        } else {
-            System.out.println("Por favor, digite um número inteiro válido.");
-        }
 
-        leitura.close();
+            leitura.close();
+        }
     }
-}
+
+
