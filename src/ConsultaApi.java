d56805e108750d9245f8a0b41f31fbe99623ef93
Erivan Muniz Machado <erivanmuniz@outlook.com>
2024-02-12 17:08:20 -0300

    Criando classe de consulta a API Fakestore

---
+++ src/ConsultaApi.java
@@ -0,0 +1,23 @@
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+
+public class ConsultaApi{
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        String endereco = "https://fakestoreapi.com/products/1";
+        System.out.println(endereco);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endereco)).build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+    }
+}
+
