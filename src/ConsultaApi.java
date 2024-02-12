3a9a953eb7b5975d79a7f110a5ae0a1a753776cb
Erivan Muniz Machado <erivanmuniz@outlook.com>
2024-02-12 17:36:23 -0300

    Alterando a Api para uma consulta dinâmica

--- src/ConsultaApi.java
+++ src/ConsultaApi.java
@@ -4,20 +4,34 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class ConsultaApi {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o número da API externa: ");
 
 
-public class ConsultaApi{
+        try {
+            int numero = scanner.nextInt();long
+            String endereco = "https://fakestoreapi.com/products/" + numero;
+            System.out.println(endereco);
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endereco)).build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println("Resultado da consulta:");
+            System.out.println(response.body());
 
-        String endereco = "https://fakestoreapi.com/products/1";
-        System.out.println(endereco);
-
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endereco)).build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
-        System.out.println(response.body());
+        } catch (Exception e) {
+            //não aparecerá mensagem
+            System.out.println("Erro: dados não encontrados : " + e.getMessage());
+        } finally {
+            scanner.close();
+            
+        }
     }
 }
 
